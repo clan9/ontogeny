7300ce23b939b0a07800aa936d5078280cd17ff9
clan9 <sglennie289@gmail.com>
2019-10-14 13:09:17 +0100

    Add Menu component and tests

---
+++ client/src/components/Menu/Menu.test.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, testStore, checkProps } from "../../utils/testUtils";
+import ConnectedMenu, { Menu } from "./index";
+
+const setup = (initialState = {}) => {
+  const store = testStore(initialState);
+  const wrapper = shallow(<ConnectedMenu store={store} />)
+    .dive()
+    .dive();
+  return wrapper;
+};
+
+describe("Menu component", () => {
+  describe("Component rendering (connected component, authorised user)", () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = setup({
+        user: {
+          isAuthenticated: true,
+          user: {
+            name: "Simon"
+          }
+        }
+      });
+    });
+
+    test("should render without error", () => {
+      const component = findByTestAttr(wrapper, "menu-component");
+      expect(component.length).toBe(1);
+    });
+
+    test("should render header", () => {
+      const header = findByTestAttr(wrapper, "header");
+      expect(header.length).toBe(1);
+    });
+
+    test("should render paragraph", () => {
+      const para = findByTestAttr(wrapper, "paragraph");
+      expect(para.length).toBe(1);
+    });
+
+    test("should render link to expenses page", () => {
+      const expenseLink = findByTestAttr(wrapper, "expenseLink");
+      expect(expenseLink.length).toBe(1);
+    });
+
+    test("should render link to income page", () => {
+      const incomeLink = findByTestAttr(wrapper, "incomeLink");
+      expect(incomeLink.length).toBe(1);
+    });
+
+    test("should render link to dashboard page", () => {
+      const dashboardLink = findByTestAttr(wrapper, "dashboardLink");
+      expect(dashboardLink.length).toBe(1);
+    });
+  });
+
+  describe("Component rendering (connected component - user NOT authorised)", () => {
+    test("should not render Menu component and redirect instead, if user is not authorised", () => {
+      const wrapper = setup({
+        user: {
+          isAuthenticated: false,
+          user: {
+            name: "Simon"
+          }
+        }
+      });
+      const component = findByTestAttr(wrapper, "menu-component");
+      expect(component.length).toBe(0);
+      const redirect = findByTestAttr(wrapper, "redirect");
+      expect(redirect.length).toBe(1);
+    });
+  });
+
+  // UNCONNECTED COMPONENT
+  describe("Prop types", () => {
+    test("should not throw warning with expected props", () => {
+      const expectedProps = {
+        isAuthenticated: true,
+        name: "Simon"
+      };
+      checkProps(Menu, expectedProps);
+    });
+  });
+});
--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -1,12 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default class Menu extends Component {
-  static propTypes = {
-    prop: PropTypes
-  };
+export const Menu = props => {
+  return props.isAuthenticated ? (
+    <div data-test="menu-component">
+      <header data-test="header">
+        <h3>Welcome {props.name}</h3>
+      </header>
 
-  render() {
-    return <div>This is the menu page</div>;
-  }
-}
+      <p data-test="paragraph">What would you like to do?</p>
+      <div>
+        <Link to="/expenses" data-test="expenseLink">
+          Go to expenses page
+        </Link>
+        <Link to="/income" data-test="incomeLink">
+          Go to income page
+        </Link>
+        <Link to="/dash" data-test="dashboardLink">
+          Go to dashboard page
+        </Link>
+      </div>
+    </div>
+  ) : (
+    <Redirect to="/" data-test="redirect" />
+  );
+};
+
+Menu.propTypes = {
+  isAuthenticated: PropTypes.bool,
+  name: PropTypes.string
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.user.isAuthenticated,
+  name: state.user.user.name
+});
+
+export default connect(mapStateToProps)(Menu);
