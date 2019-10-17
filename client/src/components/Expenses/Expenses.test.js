5259145b156cb135a0fac2b0885d74d5da417d33
clan9 <sglennie289@gmail.com>
2019-10-17 14:38:46 +0100

    Add menu link to Expenses component.  Add tests for Expenses component.

---
+++ client/src/components/Expenses/Expenses.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr } from "../../utils/testUtils";
+import Expenses from "./index";
+
+describe("Expenses Component", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Expenses />);
+  });
+
+  test("should render component without error", () => {
+    const component = findByTestAttr(wrapper, "expenses-component");
+    expect(component.length).toBe(1);
+  });
+
+  test("should render summary component", () => {
+    const summary = findByTestAttr(wrapper, "summary");
+    expect(summary.length).toBe(1);
+  });
+
+  test("should render filters component", () => {
+    const filters = findByTestAttr(wrapper, "filters");
+    expect(filters.length).toBe(1);
+  });
+
+  test("should render list component", () => {
+    const list = findByTestAttr(wrapper, "list");
+    expect(list.length).toBe(1);
+  });
+
+  test("should render menu link", () => {
+    const link = findByTestAttr(wrapper, "menu-link");
+    expect(link.length).toBe(1);
+  });
+});
--- client/src/components/Expenses/index.js
+++ client/src/components/Expenses/index.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ExpensesSummary from "../ExpensesSummary";
 import ExpenseListFilters from "../ExpenseListFilters";
 import ExpenseList from "../ExpenseList";
 
 const Expenses = () => {
   return (
-    <div>
-      <ExpensesSummary />
-      <ExpenseListFilters />
-      <ExpenseList />
+    <div data-test="expenses-component">
+      <ExpensesSummary data-test="summary" />
+      <ExpenseListFilters data-test="filters" />
+      <ExpenseList data-test="list" />
+      <Link to="/menu" data-test="menu-link">
+        Main Menu
+      </Link>
     </div>
   );
 };
