8763020806d52ee9d1b0b3dd57e2732ecd0fdd84
clan9 <sglennie289@gmail.com>
2019-10-26 15:05:24 +0100

    Change to date sorting function to take account of date ranges over more than one month (sort by timestamp an then format to show correctly in charts).

--- client/src/selectors/sortDatesAndSumTotals.js
+++ client/src/selectors/sortDatesAndSumTotals.js
@@ -3,15 +3,11 @@ import moment from "moment";
 // map through expenses and incomes for their dates
 // return an array of the dates with duplicates removed
 const sortDates = (expenses, income) => {
-  const expensesDates = expenses.map(expense =>
-    moment(expense.date).format("DD MMM")
-  );
-  const incomeDates = income.map(income =>
-    moment(income.date).format("DD MMM")
-  );
+  const expensesDates = expenses.map(expense => moment(expense.date).valueOf());
+  const incomeDates = income.map(income => moment(income.date).valueOf());
   const datesSet = new Set([...expensesDates, ...incomeDates]);
   const datesArray = [...datesSet].sort();
-  return datesArray;
+  return datesArray.map(date => moment(date).format("DD MMM"));
 };
 
 // Call the sortDates method, map through the dates
--- client/src/selectors/sortDatesAndSumTotals.test.js
+++ client/src/selectors/sortDatesAndSumTotals.test.js
@@ -4,9 +4,9 @@ import expenses from "../fixtures/expenses";
 import income from "../fixtures/income";
 
 describe("sortDatesAndSumTotals selector", () => {
-  test("sortDates -> should take in expenses and income arrays and return an array of the dates (sorted by numeric day i.e. 01..10..20 etc) with duplicates removed", () => {
+  test("sortDates -> should take in expenses and income arrays and return an array of the dates (sorted by oldest -> newest) with duplicates removed", () => {
     const result = sortDates(expenses, income);
-    const expectedResult = ["04 Jan", "06 Jan", "29 Dec"];
+    const expectedResult = ["29 Dec", "04 Jan", "06 Jan"];
     expect(result.length).toBe(3);
     expect(result).toEqual(expectedResult);
   });
@@ -15,14 +15,14 @@ describe("sortDatesAndSumTotals selector", () => {
     const result = summedTotalsForDates(expenses, income);
     const expectedResult = {
       summedExpenses: [
+        { date: "29 Dec", expenseTotal: 25 },
         { date: "04 Jan", expenseTotal: 1.95 },
-        { date: "06 Jan", expenseTotal: 595.99 },
-        { date: "29 Dec", expenseTotal: 25 }
+        { date: "06 Jan", expenseTotal: 595.99 }
       ],
       summedIncome: [
+        { date: "29 Dec", incomeTotal: 995 },
         { date: "04 Jan", incomeTotal: 20 },
-        { date: "06 Jan", incomeTotal: 595.99 },
-        { date: "29 Dec", incomeTotal: 995 }
+        { date: "06 Jan", incomeTotal: 595.99 }
       ]
     };
     expect(result).toEqual(expectedResult);
