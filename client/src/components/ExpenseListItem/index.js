316e5c324b0a5eb21d1cfd1d03e7c946a64f3c4f
clan9 <sglennie289@gmail.com>
2019-11-01 11:44:29 +0000

    Add styles for ExpenseListItem.

--- client/src/components/ExpenseListItem/index.js
+++ client/src/components/ExpenseListItem/index.js
@@ -3,20 +3,27 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import numeral from "numeral";
+import "./styles.scss";
 
 require("numeral/locales/en-gb");
 numeral.locale("en-gb");
 
 const ExpenseListItem = ({ expense }) => {
   return (
-    <Link data-test="expense-component" to={`/editExpense/${expense._id}`}>
+    <Link
+      data-test="expense-component"
+      to={`/editExpense/${expense._id}`}
+      className="record-item"
+    >
       <div data-test="description-date-div">
-        <h3 data-test="description">{expense.description}</h3>
-        <span data-test="date">
+        <h3 data-test="description" className="record-item__description">
+          {expense.description}
+        </h3>
+        <span data-test="date" className="record-item__date">
           {moment(expense.date).format("Do MMMM YYYY")}
         </span>
       </div>
-      <h3 data-test="amount">
+      <h3 data-test="amount" className="record-item__amount">
         {numeral(expense.amount / 100).format("$0,0.00")}
       </h3>
     </Link>
